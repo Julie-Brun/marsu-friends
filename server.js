8fba3a09bcc8699ea023c12da260708ab6c3f55e
Julie-Brun <brun.julie91@gmail.com>
2020-11-28 13:46:34 +0100

    Minor changes in server.js

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/appartoo_test',
 
 // Routes
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/dist', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client/dist/front', 'index.html'));
 });
 
 app.use('/auth', auth);
